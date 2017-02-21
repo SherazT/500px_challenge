dc0342db88a717485f5e8531924746eefec531ea
Sheraz Tariq <sheraztariq@hotmail.com>
2017-02-21 12:40:59 -0500

    import tf and run sess

--- mnist_conv.py
+++ mnist_conv.py
@@ -1,3 +1,17 @@
+from tensorflow.examples.tutorials.mnist import input_data
+import tensorflow as tf
+
+mnist = input_data.read_data_sets('MNIST_data', one_hot=True)
+sess = tf.InteractiveSession()
+
+x = tf.placeholder(tf.float32, shape=[None, 784])
+y_ = tf.placeholder(tf.float32, shape=[None, 10])
+
+W = tf.Variable(tf.zeros([784,10]))
+b = tf.Variable(tf.zeros([10]))
+
+sess.run(tf.initialize_all_variables())
+
 def weight_variable(shape):
   initial = tf.truncated_normal(shape, stddev=0.1)
   return tf.Variable(initial)
